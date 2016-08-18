66309092a9cf0264d72bbe8e882a4ec54823ca2d
James Stephenson <jstephen@scc-i.com>
2016-08-18 14:24:15 -0600

    Add helper for active page

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,12 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-<<<<<<< HEAD
-      user = User.create(first_name: "Mickey", last_name: "Mouse", email: "test@test.com", password: "mickey", password_confirmation: "mickey")
-=======
-      user = User.create(first_name: "Jordan", last_name: "Hudgens", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
->>>>>>> 64227bc94451849cf6cde79de521e51c28cab824
-      @post = Post.create(date: Date.today, rationale: "anything", user_id: user.id)
+      user = User.create(first_name: "Mickey", last_name: "Mouse", email: "test@test.com", password: "mickey", password_confirmation: "mickey")     
+      @post = Post.create(date: Date.today, rationale: "model login", user_id: user.id)
     end
 
     it 'can be created' do
