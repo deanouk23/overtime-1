d7cb32f9c6b333564e527daa9fca54b4390a8ff0
jordanhudgens <jordan@devcamp.com>
2016-08-18 16:56:40 +0000

    Fixed spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "anything")
+      user = User.create(first_name: "Jordan", last_name: "Hudgens", email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      @post = Post.create(date: Date.today, rationale: "anything", user_id: user.id)
     end
 
     it 'can be created' do
